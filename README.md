dc969057ef28bbc9897cd04be40bff213498b158
Ayush Raj <ayushraj.work.dev@gmail.com>
2024-06-01 19:51:28 +0530

    1. gemini-api-call added 2. ReadME instruction added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Bat-Bot
  Talk to Batman!
+
+# Gemini API Key
+ Run ```# os.environ['GOOGLE_API_KEY'] = str("your-api-key")``` to store your gemini api key
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from langchain_google_genai import GoogleGenerativeAI
+import google.generativeai as genai
 import sys
+import os
 
 app = Flask(__name__)
 CORS(app)
-
 
 # Define a function you want to use in your frontend
 def some_python_function(data):
@@ -13,13 +14,20 @@ def some_python_function(data):
     return data[::-1]
 
 def talk_to_gemini(user_prompt):
-    return ;
+    apikey = os.environ.get("GOOGLE_API_KEY")
+    if apikey is None:
+        raise ValueError("GOOGLE_API_KEY is not set in the environment.")
+    genai.configure(api_key=apikey)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(user_prompt)
+    return response.text;
 
 @app.route('/api/process', methods=['POST'])
 def process():
     data = request.json
     input_text = data.get('input')
-    result = some_python_function(input_text)
+    # result = some_python_function(input_text)
+    result = talk_to_gemini(input_text)
     return jsonify({'result': result})
 
 if __name__ == '__main__':
