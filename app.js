9abf2b8a3fd5e73a26e272b9c36c5a5ae163165e
Ayush Raj <ayushraj.work.dev@gmail.com>
2024-06-01 17:28:52 +0530

    1. virtual env added 2. api calls changed 3. requirements.txt file added

--- app.js
+++ app.js
@@ -1,49 +1,19 @@
-
 // first we fetched all the variables ( INPUT , OUTPUT , BUTTON ) required ahead 
 var btntranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
 
-// we fetched the server url , to which we want to talk or get our data
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
-
-
-// then we defined a function for whenever we want to convert a text or use server URL for the text
-function getTranslationURL(text){
-    var encodedText = encodeURI(text);
-    return serverURL + "?" + "text=" + encodedText
-}
-
 // there can be error sometimes , so a function for handling errors
 function errorHandler(error){
     console.log("error occured",error);
-    alert("Too Many Requests! Rate limit of 5 requests per hour exceeded. Please try again after sometime");
+    alert("There has been a problem with your fetch operation; Error processing request");
 }
 
 
 // what to do when button clicks happens..
-function clickHandler(){
-    // read the input
-    var input = textInput.value; 
-
-    // calling server for processing
-    var finalURL = getTranslationURL(input);
-    
-    // go fetch the response we got from the server --> then convert it to json --> 
-    // then  get that translated text and show this as the main output
-    fetch(finalURL)
-        .then( response => response.json() )
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; //main output
-        })
-        .catch(errorHandler) 
-
-}
-
 async function processText() {
-    const inputText = document.getElementById('inputText').value;
+    const inputText = textInput.value;
     try {
         const response = await fetch('http://127.0.0.1:5000/api/process', {
             method: 'POST',
@@ -58,15 +28,15 @@ async function processText() {
         }
 
         const data = await response.json();
-        document.getElementById('result').textContent = data.result;
+        // document.getElementById('result').textContent = data.result;
+        outputDiv.innerText = data.result;
     } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-        document.getElementById('result').textContent = 'Error processing request';
+        errorHandler(error);
     }
 }
 
 
 // what to do when a click happens...
-btntranslate.addEventListener("click" , clickHandler);
+btntranslate.addEventListener("click" , processText);
 
 
