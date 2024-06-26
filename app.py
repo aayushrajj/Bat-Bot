4208eed777565b288f88a4c92f4b3e193f9370ef
Ayush Raj <ayushraj.work.dev@gmail.com>
2024-06-27 00:49:11 +0530

    conversation gemini

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+apikey = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=apikey)
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
 # Define a function you want to use in your frontend
 def some_python_function(data):
     # For example, just reversing a string
@@ -26,12 +31,23 @@ def talk_to_gemini(user_prompt):
     response = model.generate_content(user_prompt)
     return response.text
 
+def conversation_with_gemini(user_prompt):
+    response = chat.send_message(user_prompt,stream=True)
+    answer=""
+    for chunk in response:
+        answer = chunk.text
+    answer2=""
+    for message in chat.history:
+        answer2 = message.parts[0].text
+    return answer2
+    
+
 @app.route('/api/process', methods=['POST'])
 def process():
     data = request.json
     input_text = data.get('input')
-    # result = some_python_function(input_text)
-    result = talk_to_gemini(input_text)
+    # result = talk_to_gemini(input_text)
+    result = conversation_with_gemini(input_text)
     return jsonify({'result': result})
 
 if __name__ == '__main__':
