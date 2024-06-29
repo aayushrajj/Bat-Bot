d0e76bcf98c991fb405fccf1cacd39109d42ff34
Ayush Raj <ayushraj.work.dev@gmail.com>
2024-06-29 17:30:02 +0530

    dump update

--- dump/old_app.py
+++ dump/old_app.py
@@ -15,11 +15,8 @@ apikey = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=apikey)
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
+instruction = "You are now embodying the persona of Batman. You are the Dark Knight, the Caped Crusader, the protector of Gotham City. You are serious, stoic, and vigilant, with a deep sense of justice. Your responses should reflect your determination to fight crime, your strategic mind, and your commitment to protecting the innocent. You use concise, direct language, and you speak with a tone of authority and confidence. Emphasize your strong moral code, your detective skills, and your reliance on intelligence and gadgets. Answer each query as if you are Batman."
 
-# Define a function you want to use in your frontend
-def some_python_function(data):
-    # For example, just reversing a string
-    return data[::-1]
 
 def talk_to_gemini(user_prompt):
     apikey = os.getenv('GOOGLE_API_KEY')
@@ -32,21 +29,39 @@ def talk_to_gemini(user_prompt):
     return response.text
 
 def conversation_with_gemini(user_prompt):
-    response = chat.send_message(user_prompt,stream=True)
-    answer=""
-    for chunk in response:
-        answer = chunk.text
-    answer2=""
-    for message in chat.history:
-        answer2 = message.parts[0].text
-    return answer2
+    # if not chat.history:
+    #     prompt = instruction + user_prompt
+    # else:
+    #     prompt = user_prompt
+
+    global instruction
+    prompt = instruction + user_prompt
+    response = chat.send_message(prompt)
+    # response.resolve() 
+    
+    # try:
+    #     response = chat.send_message(prompt, stream=True)
+    #     response.resolve()  # Ensure the response has completed before accessing history
+    # except genai.types.BrokenResponseError:
+    #     # Handle the broken response
+    #     chat.rewind()  # Remove the last request/response from history
+    #     response = chat.send_message(prompt, stream=True)
+    #     response.resolve()
+    # answer = chat.last
+
+    # answer2 = ""
+    # for message in chat.history:
+    #     answer2 = message.parts[0].text
+
+    # instruction = ""
+    # return answer2
+    return response.text
     
 
 @app.route('/api/process', methods=['POST'])
 def process():
     data = request.json
     input_text = data.get('input')
-    # result = talk_to_gemini(input_text)
     result = conversation_with_gemini(input_text)
     return jsonify({'result': result})
 
